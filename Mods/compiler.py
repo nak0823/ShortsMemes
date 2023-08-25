e0cfdc90c770a46a2a340d06dd81170236deaf67
Serial <130221150+nak0823@users.noreply.github.com>
2023-08-25 19:41:49 +0200

    Revert "bug fixed + more"

--- Mods/compiler.py
+++ Mods/compiler.py
@@ -11,25 +11,6 @@ import main as main
 
 compiled_videos = []
 
-def get_random_file_from_directory(directory_path, extensions=None):
-    """Get a random file from the specified directory with optional file extension filtering."""
-    if extensions:
-        files = [
-            f for f in os.listdir(directory_path) 
-                if os.path.isfile(os.path.join(directory_path, f)) 
-            and any(f.lower().endswith(ext.lower()) for ext in extensions)
-        ]
-    else:
-        print(os.listdir(directory_path))
-        files = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
-    
-    if not files:
-        print(os.listdir(directory_path))
-
-        raise ValueError(f"No suitable files found in directory: {directory_path}")
-
-    return os.path.join(directory_path, random.choice(files))
-
 
 def compile(is_auto):
     os.system("cls")
@@ -40,6 +21,7 @@ def compile(is_auto):
     music_paths = config.music_paths
     output_path = config.output_path
     memes_path = config.memes_path
+    output_path = config.output_path
     clip_fps = config.clip_fps
     clip_duration = config.clip_duration
     padding_top = config.padding_top
@@ -49,6 +31,7 @@ def compile(is_auto):
     # Create the needed output directories
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+
     if not os.path.exists(memes_path):
         os.makedirs(memes_path)
 
@@ -59,19 +42,17 @@ def compile(is_auto):
     # Print a summarization of the current settings.
     logger.prefix_stats(f"Shorts Fps: {logger.Colors.cyan}{clip_fps}")
     logger.prefix_stats(f"Shorts Duration: {logger.Colors.cyan}{clip_duration} sec")
-    logger.prefix_stats(f"Starting Compiling {logger.Colors.cyan}{amount_to_make}{logger.Colors.white} Shorts")
+    logger.prefix_stats(
+        f"Starting Compiling {logger.Colors.cyan}{amount_to_make}{logger.Colors.white} Shorts"
+    )
     print()
 
     for i in range(amount_to_make):
         # Time variable to calculate the time of creation.
         start_time = time.time()
 
-        # Get random background and music from their respective directories
-        random_background = get_random_file_from_directory(background_paths[0], extensions=['.mp4'])
-        random_music = get_random_file_from_directory(music_paths[0], extensions=['.mp3'])
-
-        # Set the background using the random background.
-        short = VideoFileClip(random_background)
+        # Set the background by choosing a random background from Assets.
+        short = VideoFileClip(random.choice(background_paths))
 
         # Pick 2 random memes and remove them from the memes list.
         random_memes = random.sample(memes_to_use, 2)
@@ -95,40 +76,36 @@ def compile(is_auto):
         if bottom_meme.size[0] > target_width:
             bottom_meme = bottom_meme.resize(width=target_width)
 
-        top_meme_pos = (short.size[0] // 2 - top_meme.size[0] // 2, padding_top + target_height // 2 - top_meme.size[1] // 2,)
-        bottom_meme_pos = (short.size[0] // 2 - bottom_meme.size[0] // 2, short.size[1] - padding_bottom - target_height // 2 - bottom_meme.size[1] // 2,)
+        top_meme_pos = (
+            short.size[0] // 2 - top_meme.size[0] // 2,
+            padding_top + target_height // 2 - top_meme.size[1] // 2,
+        )
+
+        bottom_meme_pos = (
+            short.size[0] // 2 - bottom_meme.size[0] // 2,
+            short.size[1]
+            - padding_bottom
+            - target_height // 2
+            - bottom_meme.size[1] // 2,
+        )
 
         # Set the memes position and duration.
         top_meme = top_meme.set_position(top_meme_pos).set_duration(clip_duration)
-        bottom_meme = bottom_meme.set_position(bottom_meme_pos).set_duration(clip_duration)
+        bottom_meme = bottom_meme.set_position(bottom_meme_pos).set_duration(
+            clip_duration
+        )
 
-        # Set a random audio under the video.
-        comp = CompositeVideoClip([short.subclip(0, clip_duration), top_meme, bottom_meme])
-
-        # Start of new audio handling
-        audio = AudioFileClip(random_music)
-        audio_duration = audio.duration
-
-        if audio_duration == clip_duration:
-            # Use the entire audio as is.
-            pass
-        elif audio_duration > clip_duration:
-            # Choose a random start time for the audio segment.
-            max_start_time = audio_duration - clip_duration
-            random_start_time = random.uniform(0, max_start_time)
-            audio = audio.subclip(random_start_time, random_start_time + clip_duration)
-        else:  # If the audio is shorter than the desired clip duration
-            loop_count = int(clip_duration // audio_duration) + 1
-            audio = audio.fx(vfx.loop, duration=clip_duration)
-
-        comp = comp.set_audio(audio)
-        # End of new audio handling
+        # Set a random audio under the video. (Note: Audios are precompiled to be the same length as the duration).
+        comp = CompositeVideoClip(
+            [short.subclip(0, clip_duration), top_meme, bottom_meme]
+        )
+        comp = comp.set_audio(AudioFileClip(random.choice(music_paths)))
 
         # Random file name to avoid duplicates.
         random_uuid = uuid.uuid4().hex
         save_position = f"{output_path}/{title.title_maker()}.mp4"
 
-        # Write the final composition to an mp4.
+        # Write the final composition to a mp4.
         comp.write_videofile(
             save_position,
             fps=clip_fps,
@@ -145,10 +122,14 @@ def compile(is_auto):
         os.remove(random_memes[0])
         os.remove(random_memes[1])
 
-        logger.prefix_stats(f"Finished Writing Short in {logger.Colors.cyan}{elapsed_time}{logger.Colors.white} seconds. {logger.Colors.red}({i + 1}/{amount_to_make})")
+        logger.prefix_stats(
+            f"Finished Writing Short # {logger.Colors.cyan}{i + 1}{logger.Colors.white}. Time Taken: {logger.Colors.cyan}{elapsed_time}"
+        )
+        logger.console_title(
+            f"Multi Functional Shorts Tool by Serialized - Creating Shorts {i}/{amount_to_make}"
+        )
 
     if not is_auto:
         logger.prefix_stats(f"Finished Writing All Shorts. Press any key to return")
         input()
         main.main()
-
